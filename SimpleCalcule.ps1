7a0834ce36833371ab07b3d08c81d4131c3af62b
gael-schooled <gael.green@student.hel.be>
2024-11-11 14:07:56 +0100

    quatrième version ajoute le PPCM et PGCD

--- SimpleCalcule.ps1
+++ SimpleCalcule.ps1
@@ -1,4 +1,24 @@
-﻿# Fonction principale de la calculatrice
+﻿# Fonction pour calculer le PGCD (Plus Grand Commun Diviseur)
+function Calculer-PGCD {
+    param([int]$a, [int]$b)
+
+    while ($b -ne 0) {
+        $temp = $b
+        $b = $a % $b
+        $a = $temp
+    }
+    return $a
+}
+
+# Fonction pour calculer le PPCM (Plus Petit Commun Multiple)
+function Calculer-PPCM {
+    param([int]$a, [int]$b)
+
+    $pgcd = Calculer-PGCD $a $b
+    return [math]::Abs($a * $b) / $pgcd
+}
+
+# Fonction principale de la calculatrice
 function Calculatrice {
     while ($true) {
         # Afficher le menu
@@ -7,13 +27,15 @@ function Calculatrice {
         echo "2. Soustraction"
         echo "3. Multiplication"
         echo "4. Division"
-        echo "5. Quitter"
+        echo "5. PGCD (Plus Grand Commun Diviseur)"
+        echo "6. PPCM (Plus Petit Commun Multiple)"
+        echo "7. Quitter"
 
         # Demander à l'utilisateur de choisir une opération
         $choix = Read-Host "Entrez le numéro de l'opération"
 
-        # Si l'utilisateur choisit 5, quitter la boucle
-        if ($choix -eq 5) {
+        # Si l'utilisateur choisit 7, quitter la boucle
+        if ($choix -eq 7) {
             echo "Merci d'avoir utilisé la calculatrice. Au revoir !"
             break
         }
@@ -44,8 +66,18 @@ function Calculatrice {
                     echo "$premier / $second = $resultat"
                 }
             }
+            5 {
+                # Calcul du PGCD
+                $resultat = Calculer-PGCD $premier $second
+                echo "Le PGCD de $premier et $second est : $resultat"
+            }
+            6 {
+                # Calcul du PPCM
+                $resultat = Calculer-PPCM $premier $second
+                echo "Le PPCM de $premier et $second est : $resultat"
+            }
             default {
-                echo "Choix invalide. Veuillez entrer un numéro valide (1-5)."
+                echo "Choix invalide. Veuillez entrer un numéro valide (1-7)."
             }
         }
 
@@ -60,5 +92,3 @@ function Calculatrice {
 
 # Lancer la calculatrice
 Calculatrice
-
-
