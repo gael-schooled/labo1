4ede6f50e2a2fbca1b52e8515992a1a9e06e89e0
gael-schooled <gael.green@student.hel.be>
2024-11-11 13:24:36 +0100

    deuxième version ajoute soustaction, division et multiplication

--- SimpleCalcule.ps1
+++ SimpleCalcule.ps1
@@ -1,6 +1,34 @@
-﻿[int]$premier = Read-Host "premier chiffre"
-[int]$second = Read-Host "second chiffre"
+﻿# Demander à l'utilisateur de saisir les deux chiffres
+[int]$premier = Read-Host "Entrez le premier chiffre"
+[int]$second = Read-Host "Entrez le second chiffre"
 
-$addition = $premier+$second
+# Demander à l'utilisateur de choisir une opération
+$choix = Read-Host "Choisissez une opération (addition, soustraction, multiplication, division)"
 
-echo $addition
+# Effectuer l'opération choisie
+switch ($choix) {
+    "addition" {
+        $resultat = $premier + $second
+        echo "$premier + $second = $resultat"
+    }
+    "soustraction" {
+        $resultat = $premier - $second
+        echo "$premier - $second = $resultat"
+    }
+    "multiplication" {
+        $resultat = $premier * $second
+        echo "$premier * $second = $resultat"
+    }
+    "division" {
+        if ($second -eq 0) {
+            echo "Erreur : Division par zéro !"
+        } else {
+            $resultat = $premier / $second
+            echo "$premier / $second = $resultat"
+        }
+    }
+    default {
+        echo "Opération invalide. Veuillez entrer 'addition', 'soustraction', 'multiplication' ou 'division'."
+    }
+}
+
